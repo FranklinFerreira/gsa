9297d52a18f909a43cb69cbb17b2b3ea5694bff4
FranklinFerreira <franklinferrreira@gmail.com>
2016-04-08 11:00:02 -0300

    Criação index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!Doctype html>
+<head></head>
+<body>
+<form method="get" action="">
+<span>Digite um numero</span><input type="text" >
+<input type="submit" value="Enviar">
+</form>
+
+
+</body>
+</html>
